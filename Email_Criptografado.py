2142da01016e3f5c6e4bd205182e98e937bc3331
ejr_geek <ejrflaut@hotmail.com>
2016-12-11 19:07:03 -0300

    Criptografar E-mail
    Apenas uma brincadeira para fazer como os amigos

---
+++ Email_Criptografado.py
@@ -0,0 +1,40 @@
+import time
+tema="""\033[1;31m  ____      _       _                         __ _       
+ / ___|_ __(_)_ __ | |_ ___   __ _ _ __ __ _ / _(_) __ _ 
+| |   | '__| | '_ \| __/ _ \ / _` | '__/ _` | |_| |/ _` |
+| |___| |  | | |_) | || (_) | (_| | | | (_| |  _| | (_| |
+ \____|_|  |_| .__/ \__\___/ \__, |_|  \__,_|_| |_|\__,_|
+             |_|             |___/                       \033[1;m"""
+
+tema2="""\033[1;34m                                                 _ _ 
+                             ___ _ __ ___   __ _(_) |
+                            / _ \ '_ ` _ \ / _` | | |
+                           |  __/ | | | | | (_| | | |
+                            \___|_| |_| |_|\__,_|_|_|
+                                                     
+"""
+print(tema);print(tema2);print("#FEITO POR > Erlon Junior\
+	#VERSAO > 2.0\
+	#CRIPTOGRAFAR EMAILS")
+email=input("\033[1;33mDigite Um E-Mail > ")
+time.sleep(1)
+print("\033[1;35mCRIPTOGRAFANDO")
+print("\033[1;35mCONECTANDO NO SERVIDOR > \
+	\033[32m⓿			0%")
+time.sleep(1.5)
+print("\033[1;35mCONECTANDO NO SERVIDOR > \
+	\033[32m⓿ ⓿ ⓿			25%")
+time.sleep(1.5)
+print("\033[1;35mCONECTANDO NO SERVIDOR > \
+	\033[32m⓿ ⓿ ⓿ ⓿ ⓿ ⓿ ⓿		75%")
+time.sleep(1.5)
+print("\033[1;35mCONECTANDO NO SERVIDOR > \
+	\033[32m⓿ ⓿ ⓿ ⓿ ⓿ ⓿ ⓿ ⓿ ⓿ ⓿	100%")
+print("\033[1;31mE-MAIL CRIPTOGRADADO COM SUCESSO!!!")
+print("PREPARANDO PARA RECEBER TOKENS DE ACESSO")
+time.sleep(0.5)
+print("VERIFICANDO CONTAS USADAS")
+time.sleep(1)
+print("\033[35mINSTAGRAM	>	\033[36mwww.instagram.com	>	\033[32mAndroid")
+print("\033[35mFACEBOOK	>	\033[36mwww.facebook.com	>	\033[32mAndroid")
+print("\033[35mFirefox Linux	>	\033[36m127.0.0.0		>	\033[32mServidor Criptografado")
